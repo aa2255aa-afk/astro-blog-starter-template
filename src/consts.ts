5327f4d9584c86ea936d8245468594fc18ead98e
M7AMY7O <aa2255aa@gmail.com>
2026-01-20 03:00:04 +0300

    تحديث SITE_TITLE و SITE_DESCRIPTION إلى 'تطبيق رواد الرياده'

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "تطبيق رواد الريادة";
+export const SITE_DESCRIPTION = "مرحبًا بكم في تطبيق رواد الريادة!";
